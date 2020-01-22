b901c720479ed94228292e0553261c6ba56a5e77
mitya <dmitriy.akhmediyev@gmail.com>
2020-01-22 20:51:06 +0300

    custom layer

--- cv_algtop/cifar_algtop2.py
+++ cv_algtop/cifar_algtop2.py
@@ -36,16 +36,16 @@ class GraphFeatures(Layer):
         self.output_dim = output_dim
         super(GraphFeatures, self).__init__(**kwargs)
 
-    def build(self, input_shape):
-        # Create a trainable weight variable for this layer.
-        self.kernel = self.add_weight(name='kernel',
-                                      shape=(input_shape[1], self.output_dim),
-                                      initializer='uniform',
-                                      trainable=True)
-        super(GraphFeatures, self).build(input_shape)  # Be sure to call this at the end
+  #  def build(self, input_shape):
+  #      # Create a trainable weight variable for this layer.
+  #      self.kernel = self.add_weight(name='kernel',
+  #                                    shape=(input_shape[1], self.output_dim),
+  #                                    initializer='uniform',
+  #                                    trainable=True)
+  #      super(GraphFeatures, self).build(input_shape)  # Be sure to call this at the end
 
-    def call(self, x):
-        return VietorisRipsComplex.clustering(x, self.kernel)
+    def call(x):
+        return VietorisRipsComplex.clustering(x)
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.output_dim)
